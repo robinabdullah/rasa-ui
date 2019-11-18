11198c5c1e19baf2c3d34d9fa27f01680b544c6b
robinabdullah <robinabdullah@yahoo.com>
2019-11-19 01:54:53 +0600

    creaet bot with bot id

--- server/db/bots.js
+++ server/db/bots.js
@@ -25,13 +25,13 @@ function getSingleBot(req, res, next) {
 
 function createBot(req, res, next) {
   logger.winston.info('Bot.createBot');
-  db.run('insert into bots(bot_name, bot_config, output_folder)' + 'values (?,?,?)', [req.body.bot_name, req.body.bot_config, req.body.output_folder], function(err) {
-    if (err) {
-      logger.winston.error("Error inserting a new record");
-    } else {
-      res.status(200).json({ status: 'success', message: 'Inserted' });
-    }
-  });
+    db.run('insert into bots(bot_id, bot_name, bot_config, output_folder)' + 'values (?,?,?,?)', [req.body.bot_id, req.body.bot_name, req.body.bot_config, req.body.output_folder], function(err) {
+      if (err) {
+        logger.winston.error("Error inserting a new record");
+      } else {
+        res.status(200).json({ status: 'success', message: 'Inserted' });
+      }
+    });
 }
 
 function updateBot(req, res, next) {
